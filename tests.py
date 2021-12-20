bff14dbfd4540a540c8718a7498181b04905e405
mskaynova <91434531+mskaynova@users.noreply.github.com>
2021-12-20 17:02:00 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,22 @@
+import unittest
+import time
+from telethon import TelegramClient
 
+api_id = int('')
+api_hash = ''
+client = TelegramClient('name', api_id, api_hash)
+
+client.start()
+class TG_test(unittest.TestCase):
+    def teststart(self):
+        try:
+            client.send_message('@LiteratureMiemBot', '/start')
+            time.sleep(3)
+            messages = client.get_messages('@LiteratureMiemBot')
+            for message in client.get_messages('@LiteratureMiemBot', limit=1):
+                mes = message.message
+            self.assertEqual(len(messages), 1)
+            text = "Привет! Рада, что ты решил(а) воспользоваться LiteratureBot!! \n\nЭтот бот предназначен для того, чтобы проверить свои знания в области литературы и узнать что-то новое для себя \n\nПомни, что при ответе на вопросы теста необходимо ввести фамилию автора с заглавной буквы. \n\nЧтобы начать, напиши /starttest \n\nУдачи!!!!"
+            self.assertRegex(mes, text)
+        except:
+            self.assertFalse(True)
