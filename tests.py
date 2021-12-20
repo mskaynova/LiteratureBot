d070926741cfae7f3c251a3a1840dd856a1dcbc8
mskaynova <91434531+mskaynova@users.noreply.github.com>
2021-12-20 17:45:32 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,12 +1,12 @@
 import unittest
 import mock
 import main
-from unittest.mock import ANY
 
 class telegrambot_test(unittest.TestCase):
 
     @mock.patch('main.bot')
     def test_hello(self, bot):
+        message = '123'
         main.hello(message)
         ret = "Привет! Рада, что ты решил(а) воспользоваться LiteratureBot!! \n\nЭтот бот предназначен для того, чтобы проверить свои знания в области литературы и узнать что-то новое для себя \n\nПомни, что при ответе на вопросы теста необходимо ввести фамилию автора с заглавной буквы. \n\nЧтобы начать, напиши /starttest \n\nУдачи!!!!"
-        bot.reply_to.assert_called_with(ret, reply_markup=ANY)
+        bot.reply_to.assert_called_with(message, ret)
