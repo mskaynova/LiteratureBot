861d8c58de34f81b7cc868a4c9668a19b3bca4ba
mskaynova <91434531+mskaynova@users.noreply.github.com>
2021-12-20 17:36:01 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,22 +1,12 @@
 import unittest
-import time
-from telethon import TelegramClient
+import mock
+import main
+from unittest.mock import ANY
 
-api_id = int('')
-api_hash = ''
-client = TelegramClient('name', api_id, api_hash)
+class telegrambot_test(unittest.TestCase):
 
-client.start()
-class TG_test(unittest.TestCase):
-    def teststart(self):
-        try:
-            client.send_message('@LiteratureMiemBot', '/start')
-            time.sleep(3)
-            messages = client.get_messages('@LiteratureMiemBot')
-            for message in client.get_messages('@LiteratureMiemBot', limit=1):
-                mes = message.message
-            self.assertEqual(len(messages), 1)
-            text = "Привет! Рада, что ты решил(а) воспользоваться LiteratureBot!! \n\nЭтот бот предназначен для того, чтобы проверить свои знания в области литературы и узнать что-то новое для себя \n\nПомни, что при ответе на вопросы теста необходимо ввести фамилию автора с заглавной буквы. \n\nЧтобы начать, напиши /starttest \n\nУдачи!!!!"
-            self.assertRegex(mes, text)
-        except:
-            self.assertFalse(True)
+    @mock.patch('main.bot')
+    def test_hello(self, bot):
+        main.hello(message)
+        ret = "Привет! Рада, что ты решил(а) воспользоваться LiteratureBot!! \n\nЭтот бот предназначен для того, чтобы проверить свои знания в области литературы и узнать что-то новое для себя \n\nПомни, что при ответе на вопросы теста необходимо ввести фамилию автора с заглавной буквы. \n\nЧтобы начать, напиши /starttest \n\nУдачи!!!!"
+        bot.reply_to.assert_called_with(ret, reply_markup=ANY)
